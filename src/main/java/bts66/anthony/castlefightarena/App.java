16c05406ce421c9f2492ce1cd5865067e296df41
adeoliveirasantos <anthony.deoliveirasantos.2024@lurcat.fr>
2026-05-16 14:12:48 +0200

    Compilation exécutable du jeu & launch4j

--- src/main/java/bts66/anthony/castlefightarena/App.java
+++ src/main/java/bts66/anthony/castlefightarena/App.java
@@ -12,6 +12,10 @@ import java.io.IOException;
 
 public class App extends Application {
 
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-view.fxml"));
---
+++ src/main/java/bts66/anthony/castlefightarena/Main.java
@@ -0,0 +1,7 @@
+package bts66.anthony.castlefightarena;
+
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
